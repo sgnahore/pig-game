65fda585d80deaadd32f4b713aedeeeb15a1eaab
Sgnahore <stephaniergnahore@gmail.com>
2023-07-26 22:30:48 +0100

    Variable names changed, function simplified, track dieNum variable instead of image

--- src/components/dice components/Dice.tsx
+++ src/components/dice components/Dice.tsx
@@ -10,56 +10,62 @@ import Six from "./images/Six.svg";
 
 export default function App(): JSX.Element {
   const diceImages = [One, Two, Three, Four, Five, Six];
-  const [image, setImage] = useState(diceImages[-1]);
-  const [renderedRanNum, setRenderedRanNum] = useState<number>(0);
-  const [heldNum, setHeldNum] = useState<number>(0);
+  const [turnTotal, setTurnTotal] = useState<number>(0);
+  const [heldScore, setHeldScore] = useState<number>(0);
+  const [dieNum, setDieNum] = useState(0);
+
+  // const arr = useState(0);
+  // const dieNum = arr[0];
+  // const setDieNum = arr[1];
+
+  const image = diceImages[dieNum - 1];
 
   const handleRollDice = () => {
-    const randomNum = Math.floor(Math.random() * 6);
-    const dieNum = randomNum + 1;
-    setImage(diceImages[randomNum]);
+    const roll = Math.floor(Math.random() * 6) + 1;
+    setDieNum(roll);
 
-    const rollSum = () => {
-      const sum = renderedRanNum + dieNum;
-      setRenderedRanNum(sum);
-
-      if (dieNum === 1) {
-        setRenderedRanNum(0);
-      }
-    };
-    console.log('Increasing by', dieNum);
-    //console.log('sum equals', renderedRanNum + dieNum)
-    rollSum();
+    const sum = turnTotal + roll;
+    setTurnTotal(sum);
+    console.log({ turnTotal, heldScore, avocado: roll });
+    if (roll === 1) {
+      setTurnTotal(0);
+    }
+    console.log("Increasing by", roll);
+    //console.log('sum equals', turnTotal + roll)
   };
 
   const handleStartAgain = () => {
-    setRenderedRanNum(0);
-    setHeldNum(0);
+    setTurnTotal(0);
+    setHeldScore(0);
   };
 
   const handleHoldNum = () => {
-    const score = heldNum + renderedRanNum;
-    setHeldNum(score);
-    setRenderedRanNum(0);
-  }
+    const score = heldScore + turnTotal;
+    setHeldScore(score);
+    setTurnTotal(0);
+  };
   return (
     <div>
       <center>
         <h1>Welcome to the pig APP!</h1>
         <div className="container">
-          {heldNum >= 100 && (
+          {heldScore >= 100 && (
             <>
               <p> winner! </p>
               <button onClick={handleStartAgain}>PLAY AGAIN</button>
             </>
           )}
-          <img className="square" src={image} alt="dice"></img>
+          {image !== undefined ? (
+            <img className="square" src={image} alt="dice"></img>
+          ) : (
+            "Roll the Die!"
+          )}
         </div>
         <button onClick={handleRollDice}>ROLL</button>
+        <p>current roll: {dieNum}</p>
         <button onClick={handleHoldNum}>HOLD</button>
-        <p> YOUR SCORE: {heldNum} </p>
-        {renderedRanNum}
-
+        <p> YOUR SCORE: {heldScore} </p>
+        {turnTotal}
       </center>
     </div>
   );
